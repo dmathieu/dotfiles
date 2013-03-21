a92623e540642548467e9a8c507f2776c5d7ff70
Damien Mathieu <42@dmathieu.com>
2013-03-21 13:49:21 +0100

    add alias to run single cucumber feature

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,7 @@ alias sc='bundle exec rails c'
 alias sg='bundle exec rails g'
 
 alias migrate='rake db:migrate db:test:clone'
+
+function feature() {
+  bundle exec cucumber $1 --require features
+}
