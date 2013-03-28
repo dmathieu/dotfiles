994008bfbd8b22590deb13938bad496556f25abc
Damien Mathieu <damien.mathieu@shazino.com>
2013-03-28 16:36:12 +0100

    finetune the garbage collector limits to increase rails' boot time

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -6,3 +6,6 @@ alias migrate='rake db:migrate db:test:clone'
 function feature() {
   bundle exec cucumber $1 --require features
 }
+
+export RUBY_GC_MALLOC_LIMIT=60000000
+export RUBY_FREE_MIN=200000
