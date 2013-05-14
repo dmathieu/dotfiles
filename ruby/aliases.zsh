ff0a5f9a3ef83040a1169c181a5f6e4d7a5b9a01
Damien Mathieu <damien.mathieu@shazino.com>
2013-05-14 09:30:15 +0200

    allow running several features

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -4,5 +4,5 @@ alias sg='bundle exec rails g'
 alias migrate='rake db:migrate db:test:clone'
 
 function feature() {
-  bundle exec cucumber $1 --require features
+  bundle exec cucumber $1 $2 $3 $4 $5 --require features
 }
