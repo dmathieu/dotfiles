2be7ec873502ff8d54cd116cc979a6a44dfa762b
Damien Mathieu <damien@heroku.com>
2015-01-14 13:46:29 +0100

    use gnu-tar as the tar command by default

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,5 @@
 export PATH=/usr/local/bin:$PATH:/usr/local/sbin
 export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
+
+# Use GNU-tar
+PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
