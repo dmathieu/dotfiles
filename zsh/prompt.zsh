495b047f75f04f8e6457afde4cdddd08f60ba096
Damien Mathieu <damien@heroku.com>
2015-09-02 15:47:34 +0200

    use git-radar

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,51 +1,4 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  st=$($git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ "$st" =~ ^nothing ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
 
 ruby_version() {
   if (( $+commands[rbenv] ))
@@ -97,7 +50,8 @@ cloud_name() {
   fi
 }
 
-export PROMPT=$'\n$(versions_prompt)in $(directory_name) $(cloud_name)$(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(versions_prompt)in $(directory_name) $(cloud_name)\n› '
+export PROMPT="$PROMPT\$(git-radar --zsh --fetch) "
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
