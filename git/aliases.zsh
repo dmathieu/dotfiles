437fd2bf8361f9369701bceb3c0d78a72fbce81f
Damien Mathieu <damien@heroku.com>
2015-09-15 18:40:09 +0200

    add gpu alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,4 +9,6 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative --all"
-alias gclean="git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d"
+alias gclean="git pull origin --rebase; git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d"
+
+alias gpu='git push origin -u `git rev-parse --abbrev-ref HEAD`'
