e5e002c023e34ba6580d44c9d0618af99490986c
Damien Mathieu <damien@heroku.com>
2015-12-01 09:44:49 +0100

    allow fetching a project with a user name

--- zsh/project.zsh
+++ zsh/project.zsh
@@ -3,25 +3,37 @@ export CODEPATH=~/code
 function project() {
   users=(heroku dmathieu)
 
-  for user in $users; do
-    dir=$CODEPATH"/src/github.com/"$user"/"$1
+  if [ ${1[(ws:/:)1]} != ${1[(ws:/:)2]} ]; then
+    stickyUser=${1[(ws:/:)1]}
+    projectName=${1[(ws:/:)2]}
+  else
+    stickyUser=
+    projectName=$1
+  fi
 
-    if [[ -d $dir ]]; then
-      cd $dir
-      return
+  for user in $users; do
+    if [[ -z $stickyUser || $stickyUser == $user ]]; then
+      dir=$CODEPATH"/src/github.com/"$user"/"$projectName
+
+      if [[ -d $dir ]]; then
+        cd $dir
+        return
+      fi
     fi
   done
 
   for user in $users; do
-    apiUri="https://api.github.com/repos/"$user"/"$1
-    gitUri="https://github.com/"$user"/"$1".git"
-    code=`curl -n -o /dev/null --silent --head --write-out '%{http_code}\n' $apiUri`
+    if [[ -z $stickyUser || $stickyUser == $user ]]; then
+      apiUri="https://api.github.com/repos/"$user"/"$projectName
+      gitUri="https://github.com/"$user"/"$projectName".git"
+      code=`curl -n -o /dev/null --silent --head --write-out '%{http_code}\n' $apiUri`
 
-    if [[ $code == 200 ]]; then
-      dir=$CODEPATH"/src/github.com/"$user"/"$1
-      git clone $gitUri $dir
-      cd $dir
-      return
+      if [[ $code == 200 ]]; then
+        dir=$CODEPATH"/src/github.com/"$user"/"$projectName
+        git clone $gitUri $dir
+        cd $dir
+        return
+      fi
     fi
   done
 
