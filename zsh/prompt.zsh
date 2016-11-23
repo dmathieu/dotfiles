4ec6f9e85f68589671c79f8d24d5750278b6267b
Damien Mathieu <damien@heroku.com>
2016-11-23 13:21:03 +0100

    remove language versions from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,42 +1,5 @@
 autoload colors && colors
 
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-node_version() {
-  if (( $+commands[node] ))
-  then
-    echo $(node --version)
-  fi
-}
-
-versions_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    ruby="%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    ruby=""
-  fi
-
-  if ! [[ -z "$(node_version)" ]]
-  then
-    node="%{$fg_bold[green]%}$(node_version)%{$reset_color%} "
-  else
-    node=""
-  fi
-
-  echo "%{$node%}%{$ruby%}"
-}
-
 directory_name() {
   dname=$(projectName)
   if [[ $dname == "N/A" ]]
@@ -56,7 +19,7 @@ cloud_name() {
   fi
 }
 
-export PROMPT=$'\n$(versions_prompt)in $(directory_name) $(cloud_name)\n› '
+export PROMPT=$'\n$(directory_name) $(cloud_name)› '
 export PROMPT="$PROMPT\$(git-radar --zsh --fetch) "
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
