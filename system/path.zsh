e5824e2b5358942215012b9a4e9aa2795edf545a
dmathieu <42@dmathieu.com>
2017-02-01 09:33:02 +0100

    no postgres.app anymore, and use gnu-tar

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH=~/.dotfiles/bin:$PATH
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
