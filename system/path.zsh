8b25629036f3ab1c3c0b33cfacbb071dedf9fa9a
Damien Mathieu <dmathieu@salesforce.com>
2020-01-27 11:32:09 +0100

    symlink python to the homebrew version

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
 export PATH=~/.dotfiles/bin:$PATH
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH=/usr/local/opt/python/libexec/bin:$PATH
