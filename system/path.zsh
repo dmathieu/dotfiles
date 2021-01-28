0cf18e46d7583ea28bff3ac21d7da9799037920f
dmathieu <dmathieu@salesforce.com>
2021-01-28 09:31:27 +0100

    add kubebuilder to path

--- system/path.zsh
+++ system/path.zsh
@@ -3,6 +3,7 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 export PATH=/usr/local/opt/python/libexec/bin:$PATH
 export PATH=~/.cargo/bin:$PATH
+export PATH=$PATH:/usr/local/kubebuilder/bin
 
 export GOPATH=~/code
 export PATH=$PATH:$GOPATH/bin
