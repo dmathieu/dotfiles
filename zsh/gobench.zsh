27b303cd5aeb5a7ae82556bfb5ddab98de8a498e
Damien Mathieu <damien.mathieu@okta.com>
2022-02-03 10:22:52 +0100

    add gobench function

---
+++ zsh/gobench.zsh
@@ -0,0 +1,29 @@
+function gobench() {
+	branch=`git rev-parse --abbrev-ref HEAD`
+
+	if [[ $* != *\ -f* ]]; then
+		rm bench-main
+	fi
+	if [ ! -f bench-main ]; then
+		if [[ $(git diff --stat) != '' ]]; then
+			echo "I need to run benchmarks on main, which requires a non-dirty repository"
+			exit 1
+		fi
+
+		echo "Running benchmarks on main branch"
+		git checkout main
+		for _ in $(seq 10); do
+			go test -run Benchmark -bench=. ./... >> bench-main
+		done
+	fi
+
+	echo "Running benchmarks on $branch"
+	git checkout $branch
+	rm bench-branch
+	for _ in $(seq 10); do
+		go test -run Benchmark -bench=. ./... >> bench-branch
+	done
+
+	echo "Running benchstat"
+	benchstat bench-main bench-branch
+}
