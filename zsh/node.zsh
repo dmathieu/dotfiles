5dbae777879738bd83b279f5e0662abe6d73d1ab
dmathieu <damien.mathieu@elastic.co>
2024-01-08 15:47:30 +0100

    use brew nvm only on darwin

--- zsh/node.zsh
+++
@@ -1,3 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
-[ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
